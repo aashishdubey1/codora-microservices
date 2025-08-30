14990313110d5e03cd669fdf524fed443b826271
Aashish <aashishdubey.dev@gmail.com>
2025-08-30 20:19:56 +0530

    feat: added zod schema validators

---
+++ problemService/src/validators/problem.validatior.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const createProblemSchema = z.object({
+  title: z.string().min(1),
+  description: z.string().min(1),
+  difficulty: z.enum(["easy", "medium", "hard"]),
+  editorial: z.string().optional(),
+  testcases: z.array(
+    z.object({ input: z.string().min(1), output: z.string().min(1) })
+  ),
+});
+
+export const updateProblemSchema = createProblemSchema.partial();
+
+export type CreateProblemDto = z.infer<typeof createProblemSchema>;
+export type UpdateProblemDto = z.infer<typeof updateProblemSchema>;
