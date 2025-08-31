3b8f74ae03b9cd05719602b63f60dfd0663d39e9
Aashish <aashishdubey.dev@gmail.com>
2025-08-31 12:50:13 +0530

    feat: added crud on problems

--- problemService/src/controller/problems.controller.ts
+++ problemService/src/controller/problems.controller.ts
@@ -1,8 +1,9 @@
-import type { Request, Response, NextFunction } from "express";
+import { type Request, type Response, type NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 
 import { ProblemRepository } from "../repositories/problems.repository";
 import { ProblemService } from "../services/problem.service";
+import type { UpdateProblemDto } from "../validators/problem.validatior";
 
 const problemService = new ProblemService(new ProblemRepository());
 
@@ -11,29 +12,115 @@ export class ProblemController {
     req: Request,
     res: Response,
     next: NextFunction
-  ): Promise<void> {}
+  ): Promise<void> {
+    try {
+      const problem = await problemService.createProblem(req.body);
+
+      res.status(StatusCodes.CREATED).json({
+        success: true,
+        message: "Problem added",
+        data: problem,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 
   async getProblemById(
-    req: Request,
+    req: Request<{ id: string }, {}>,
     res: Response,
     next: NextFunction
-  ): Promise<void> {}
+  ): Promise<void> {
+    try {
+      const problem = await problemService.getProblemById(req.params.id);
+
+      if (!problem) {
+        res.status(StatusCodes.NOT_FOUND).json({
+          success: false,
+          message: "Problem not found",
+          data: problem,
+        });
+        return;
+      }
+
+      res.status(StatusCodes.OK).json({
+        success: true,
+        message: "Problem fetched successfully",
+        data: problem,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 
   async getAllProblem(
     req: Request,
     res: Response,
     next: NextFunction
-  ): Promise<void> {}
+  ): Promise<void> {
+    try {
+      const problems = await problemService.getAllProblems();
+
+      res.status(StatusCodes.OK).json({
+        success: true,
+        message: "Problem fetched successfully",
+        data: problems,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 
   async updateProblem(
-    req: Request,
+    req: Request<{ id: string }, {}, UpdateProblemDto>,
     res: Response,
     next: NextFunction
-  ): Promise<void> {}
+  ): Promise<void> {
+    const id = req.params.id;
+    const body = req.body;
+
+    try {
+      const updatedProblem = await problemService.udpateProblem(id, body);
+
+      if (!updatedProblem) {
+        res.status(StatusCodes.NOT_FOUND).json({
+          success: false,
+          message: "Problem not found",
+          data: updatedProblem,
+        });
+        return;
+      }
+
+      res.status(200).json({
+        message: "Problem updated successfully",
+        data: updatedProblem,
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 
   async deleteProblem(
-    req: Request,
+    req: Request<{ id: string }>,
     res: Response,
     next: NextFunction
-  ): Promise<void> {}
+  ): Promise<void> {
+    const problem = await problemService.deleteProblem(req.params.id);
+
+    if (!problem) {
+      res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: "Problem not found",
+        data: problem,
+      });
+      return;
+    }
+
+    res.status(200).json({
+      message: "Problem deleted successfully",
+      data: problem,
+      success: true,
+    });
+  }
 }
